4bb0fd47db1fbb61c722323e807232eb1b80e926
csmahmud99 <csmahmud.2021@gmail.com>
2023-05-21 02:04:26 +0600

    POST API is written for sending data to MongoDB through server from client

--- index.js
+++ index.js
@@ -29,12 +29,23 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const addedToyCollection = client.db("toyMarket").collection("addedToys");
+
+        app.post("/addToys", async (req, res) => {
+            const addToy = req.body;
+            console.log(addToy);
+
+            const result = await addedToyCollection.insertOne(addToy);
+            res.send(result);
+        });
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
